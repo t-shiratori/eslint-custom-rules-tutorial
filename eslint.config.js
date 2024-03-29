c84cb779d0ba59d8e2aff9c3632af56a4d833350
takashi <tks.srtr89@gmail.com>
2024-03-29 19:59:57 +0900

    add: eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+// eslint.config.js
+"use strict";
+
+// Import the ESLint plugin locally
+const eslintPluginExample = require("./eslint-plugin-example");
+
+module.exports = [
+    {
+        files: ["**/*.js"],
+        languageOptions: {
+            sourceType: "commonjs",
+            ecmaVersion: "latest",
+        },
+        // Using the eslint-plugin-example plugin defined locally
+        plugins: {"example": eslintPluginExample},
+        rules: {
+            "example/enforce-foo-bar": "error",
+        },
+    }
+]
