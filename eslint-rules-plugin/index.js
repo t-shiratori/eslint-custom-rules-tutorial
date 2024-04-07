a461644e7ca382d05ca949ed964f9227e3787144
takashi <tks.srtr89@gmail.com>
2024-04-07 23:16:32 +0900

    update

--- eslint-rules-plugin/index.js
+++ eslint-rules-plugin/index.js
@@ -1,3 +1,9 @@
 import { enforceFooBarRule } from './enforce-foo-bar.js'
-const plugin = { rules: { 'enforce-foo-bar': enforceFooBarRule } }
+const plugin = {
+  meta: {
+    name: 'eslint-rules-plugin',
+    version: '1.0.0',
+  },
+  rules: { 'enforce-foo-bar': enforceFooBarRule },
+}
 export default plugin
